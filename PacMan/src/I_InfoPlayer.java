7b4ca89c99d3d875077ac72786f2da70781adb0c
Paul-Etienne Richard <paul-etienne.richard@ensimag.imag.fr>
2013-04-05 00:06:54 -0400

    qd

---
+++ PacMan/src/I_InfoPlayer.java
@@ -0,0 +1,89 @@
+import java.rmi.*;
+ 
+public interface I_InfoPlayer extends Remote {
+		 
+    public boolean getIngame() throws RemoteException;
+    
+    
+    public void setIngame(boolean b) throws RemoteException;
+    
+
+    public boolean getDying() throws RemoteException;
+    
+    
+    public void setDying(boolean b) throws RemoteException;
+    
+    
+    public int getScore() throws RemoteException;
+    
+    
+    public void setScore(int i) throws RemoteException;
+        
+    
+    public int getViewdx() throws RemoteException;
+    
+    
+    public void setViewdx(int i) throws RemoteException;
+    
+    
+    public int getViewdy() throws RemoteException;
+    
+    
+    public void setViewdy(int i) throws RemoteException;
+    
+
+	public int getPacmanx()  throws RemoteException;
+	
+
+	public void setPacmanx(int pacmanx)  throws RemoteException;
+	
+
+	public int getPacmany()  throws RemoteException;
+	
+
+	public void setPacmany(int pacmany)  throws RemoteException;
+	
+
+	public int getPacmandx()  throws RemoteException;
+	
+
+	public void setPacmandx(int pacmandx)  throws RemoteException;
+	
+
+	public int getPacmandy()  throws RemoteException;
+	
+
+	public void setPacmandy(int pacmandy)  throws RemoteException;
+	
+
+	public int getPacsleft()  throws RemoteException;
+	
+
+	public void setPacsleft(int pacsleft)  throws RemoteException;
+	
+
+	public int getReqdx()  throws RemoteException;
+	
+
+	public void setReqdx(int reqdx)  throws RemoteException;
+	
+
+	public int getReqdy()  throws RemoteException;
+	
+
+	public void setReqdy(int reqdy)  throws RemoteException;
+	
+
+	public void DrawPacMan()  throws RemoteException;
+	
+
+	public void setPlayer(Player p)  throws RemoteException;
+	
+
+	public void drawGhost(int i, int j)  throws RemoteException;
+	
+
+	public int getPacmanspeed()  throws RemoteException;
+	
+		 
+}
