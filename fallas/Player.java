f9d65cecc4808aed51575bc9c513b4f15d055f11
Thierry Lusseyran <Thierry@MacBook-Pro-de-Thierry.local>
2013-06-21 18:31:02 -0400

    plop de nouveau

--- fallas/Player.java
+++ fallas/Player.java
@@ -69,10 +69,23 @@ public class Player extends JPanel implements ActionListener {
 	Timer timer;
 
 
-	public Player(I_InfoGame g, String addr, String namePlayer) {
+	public Player(String addr, String namePlayer) {
 
-		game = g;
 		currentAddress = addr;
+        
+        try{
+			game = (I_InfoGame) Naming.lookup("rmi://"+currentAddress+":1099/I_InfoGame");
+		} catch (NotBoundException e){
+			System.out.println("El servicio no esta publicado en el servidor");
+			System.exit(128);
+		} catch (MalformedURLException e){
+			System.out.println("URL invalida");
+			System.exit(128);
+		} catch (RemoteException e){
+			System.out.println("Excepcion remota tratanod de conectarse al servidor");
+			System.exit(128);
+		}
+        
 		if (namePlayer.equals("")){
 			playerName = "Player" + (int)(Math.random()*1000);
 		}else{
